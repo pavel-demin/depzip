fd7ffe71245812fe27c7de05ac9d93c5bda1616f
Pavel Demin <pavel-demin@outlook.com>
2025-08-14 22:35:27 +0200

    initial commit

---
+++ examples/bundle.py
@@ -0,0 +1,26 @@
+import depzip
+
+depzip.bundle(
+    modules=[
+        "matplotlib.backends.backend_qtagg",
+        "mpl_toolkits.mplot3d",
+        "numpy",
+        "PySide6.QtCore",
+        "PySide6.QtGui",
+        "PySide6.QtNetwork",
+        "PySide6.QtUiTools",
+        "PySide6.QtWidgets",
+    ],
+    includes=[
+        "Lib\\site-packages\\depzip\\import.exe",
+        "Lib\\site-packages\\matplotlib\\mpl-data",
+        "Lib\\site-packages\\PySide6\\plugins\\platforms\\qwindows.dll",
+        "Lib\\site-packages\\PySide6\\plugins\\styles\\qmodernwindowsstyle.dll",
+        "Lib\\site-packages\\PySide6\\uic.exe",
+    ],
+    excludes=[
+        "python.exe",
+        "pythonw.exe",
+    ],
+    output="bundle.zip",
+)
