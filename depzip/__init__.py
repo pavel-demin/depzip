01376eb5315a6b1b88bfa169ffb74492b4ffe9ef
Pavel Demin <pavel-demin@outlook.com>
2025-08-15 22:38:02 +0200

    add license files for dependencies

--- depzip/__init__.py
+++ depzip/__init__.py
@@ -1,18 +1,20 @@
 import os
 import sys
-
-import dllist
-import importlib
-
+from dllist import dllist
+from importlib import import_module
+from importlib.metadata import distribution, packages_distributions
 from modulefinder import ModuleFinder
-
 from zipfile import ZipFile, ZIP_DEFLATED
+
+
+def contains(string, substrings):
+    return any(s.casefold() in string.casefold() for s in substrings)
 
 
 def bundle(modules=[], includes=[], excludes=[], output="bundle.zip"):
     directory = os.path.dirname(sys.executable)
 
-    bundle = set()
+    # Use ModuleFinder to track module dependencies
 
     finder = ModuleFinder()
 
@@ -21,32 +23,58 @@ def bundle(modules=[], includes=[], excludes=[], output="bundle.zip"):
     for m in modules:
         finder.import_hook(m)
 
-    for name, module in finder.modules.items():
-        f = getattr(module, "__file__", None)
-        if f is not None:
-            bundle.add(os.path.relpath(f, directory))
+    # Collect files to bundle
+
+    bundle = {v.__file__ for v in finder.modules.values() if v.__file__}
+
+    # Find and collect license files for bundled modules
+
+    packages = {k.split(".", 1)[0] for k in finder.modules.keys()}
+    mapping = packages_distributions()
+    licenses = {"Python": {os.path.join(directory, "LICENSE.txt")}}
+
+    for p in packages:
+        for d in mapping.get(p, []):
+            licenses.setdefault(d, set()).update(
+                os.path.join(r, f)
+                for r, _, files in os.walk(distribution(d)._path)
+                for f in files
+                if contains(f, ("license", "copying"))
+            )
+
+    # Import modules to load any DLLs
 
     for m in modules:
-        importlib.import_module(m)
+        import_module(m)
 
-    for dll in dllist.dllist():
-        if any(x.casefold() in dll.casefold() for x in ("VCRUNTIME", "MSVCP")):
+    # Add DLLs to the bundle
+
+    for dll in dllist():
+        if contains(os.path.basename(dll), ("vcruntime", "msvcp")):
             continue
         if os.path.commonpath([dll, directory]) == directory:
-            bundle.add(os.path.relpath(dll, directory))
+            bundle.add(dll)
 
-    for element in includes:
-        if os.path.isdir(element):
-            for root, dirs, files in os.walk(element):
+    # Include additional files and directories
+
+    for path in includes:
+        if os.path.isdir(path):
+            for root, dirs, files in os.walk(path):
                 dirs[:] = [d for d in dirs if d != "__pycache__"]
-                for f in files:
-                    bundle.add(os.path.join(root, f))
-        elif os.path.isfile(element):
-            bundle.add(element)
+                bundle.update(os.path.join(root, f) for f in files)
+        elif os.path.isfile(path):
+            bundle.add(path)
 
-    bundle -= set(excludes)
+    # Exclude specified files
+
+    bundle = {f for f in bundle if not contains(f, excludes)}
+
+    # Create the output zip file
 
     with ZipFile(output, mode="w", compression=ZIP_DEFLATED) as zf:
-        for element in sorted(bundle):
-            if os.path.isfile(element):
-                zf.write(element)
+        for name, files in licenses.items():
+            for f in sorted(files):
+                zf.write(f, os.path.join("Licenses", name, os.path.basename(f)))
+        for f in sorted(bundle):
+            if os.path.isfile(f):
+                zf.write(f, os.path.relpath(f, directory))
